dd5d74965b6b345ff90dcf4781b54354dbb0455b
Matheus Duarte <matheusduartesouza09@gmail.com>
2019-02-14 13:26:37 -0800

    setting up row for on click listener

--- app/src/main/java/com/example/starwarsapi/adapters/MainAdapter.kt
+++ app/src/main/java/com/example/starwarsapi/adapters/MainAdapter.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import com.example.starwarsapi.R
 import com.example.starwarsapi.data.models.StarWarsCatalog
+import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.film_row.view.*
 
 class MainAdapter(val catalog : StarWarsCatalog) : RecyclerView.Adapter<CustomViewHolder>() {
@@ -20,12 +21,27 @@ class MainAdapter(val catalog : StarWarsCatalog) : RecyclerView.Adapter<CustomVi
     }
 
     override fun onBindViewHolder(holder: CustomViewHolder, position: Int) {
-        val videoTile = catalog.results.get(position)
-        holder?.view?.textView_main_filmTitle?.text = videoTile.title
+        val filmTitleTextView = catalog.results.get(position)
+        holder?.view?.textView_main_filmTitle?.text = filmTitleTextView.title
+
+        val posterImageView = holder?.view?.imageView_main_filmImage
+        when(catalog.results.get(position).episode_id){
+            1 -> posterImageView.setImageResource(R.drawable.starwars_ep1_poster)
+            2 -> posterImageView.setImageResource(R.drawable.starwars_ep2_poster)
+            3 -> posterImageView.setImageResource(R.drawable.starwars_ep3_poster)
+            4 -> posterImageView.setImageResource(R.drawable.starwars_ep4_poster)
+            5 -> posterImageView.setImageResource(R.drawable.starwars_ep5_poster)
+            6 -> posterImageView.setImageResource(R.drawable.starwars_ep6_poster)
+            7 -> posterImageView.setImageResource(R.drawable.starwars_ep7_poster)
+        }
     }
 
 }
 
 class CustomViewHolder(val view: View): RecyclerView.ViewHolder(view){
+    init {
+        view.setOnClickListener {
 
+        }
+    }
 }
--- app/src/main/java/com/example/starwarsapi/data/models/Film.kt
+++ app/src/main/java/com/example/starwarsapi/data/models/Film.kt
@@ -1,3 +1,3 @@
 package com.example.starwarsapi.data.models
 
-data class Film(val title: String, val director: String,val opening_crawl: String, val characters: List<String>)
+data class Film(val title: String,val episode_id: Int, val director: String,val opening_crawl: String, val characters: List<String>)
